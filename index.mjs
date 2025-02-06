5f5a05d33e301a4b2459f8bd33538b0d756df1fc
Tudor Anghelescu <tudor@genez.io>
2025-02-06 18:30:47 +0000

    update status code

--- index.mjs
+++ index.mjs
@@ -1,11 +1,11 @@
 // see https://genezio.com/docs/tutorials/how-to-deploy-a-serverless-function/ to learn more about function usage and parameters
 
 export const handler = async (event) => {
-  console.log("Function was called");
+  console.log('Function was called');
   console.log(event);
-  const name = event.queryStringParameters?.name || "World";
+  const name = event.queryStringParameters?.name || 'World';
   return {
-    statusCode: 200,
+    statusCode: 220,
     body: `Hello, ${name}! Welcome to Genezio Functions!`,
   };
 };
